31fe7b3c0c6b1c3b7b8b479a89e58dd46dd45065
Pedro Muniz <phmunizdematos@gmail.com>
2023-06-24 19:55:16 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
 // import Typed from 'typed.js'
 
-const navBtn = document.getElementById('nav-btn')
-const navMenu = document.getElementById('nav-menu')
-const body = document.querySelector('body')
+const navBtn = document.getElementById("nav-btn")
+const navMenu = document.getElementById("nav-menu")
+const body = document.querySelector("body")
 let classNavMenu = navMenu.className
 
 console.log(classNavMenu)
 
-navBtn.addEventListener('click', toggleMenu)
+navBtn.addEventListener("click", toggleMenu)
 
 function toggleMenu() {
-  navMenu.classList.toggle('active')
+  navMenu.classList.toggle("active")
 }
-
-// var typingEffect = new Typed('.type-effect-text', {
-//   strings: ['tutores', 'alunos', 'Aprova +'],
-//   loop: true,
-//   typeSpeed: 100,
-//   backSpeed: 80,
-//   backDelay: 1500
-// })
